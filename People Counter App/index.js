3e12282cac1a1add104574718347a759667f5664
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-10-17 00:14:15 +0300

    shows counting

--- People Counter App/index.js
+++ People Counter App/index.js
@@ -1,1 +1,11 @@
-document.getElementById("count-el").innerText = 5
+let countEl = document.getElementById("count-el");
+
+console.log(countEl)
+
+let counter = 0;
+
+function increment() {
+    counter += 1;
+    countEl.innerText = counter;
+}
+
