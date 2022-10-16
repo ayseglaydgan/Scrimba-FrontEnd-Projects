316b175e947e88fc769d7871f9cac0a0e0a296a2
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-10-17 01:14:53 +0300

    People Counter App complited as draft

--- People Counter App/index.js
+++ People Counter App/index.js
@@ -1,11 +1,25 @@
 let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 console.log(countEl)
+console.log(saveEl)
 
 let counter = 0;
 
 function increment() {
     counter += 1;
-    countEl.innerText = counter;
+    countEl.textContent = counter;
 }
 
+// function reset() {
+//     counter = 0;
+//     countEl.textContent = counter;
+// }
+
+function save() {
+    let countStr = counter + " - ";
+    saveEl.textContent += countStr;
+    console.log(countStr );
+    countEl.textContent = 0;
+    counter = 0;
+}
