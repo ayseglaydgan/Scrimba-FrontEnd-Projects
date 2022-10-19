47feb0129387ef910b07c14102673f23fb9c6b30
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-10-19 17:45:46 +0300

    project optimized

--- Basketball Score Table/index.js
+++ Basketball Score Table/index.js
@@ -5,38 +5,13 @@ let guestScore = document.getElementById("guest_score");
 let score_home = 0;
 let score_guest = 0;
 
-function incrementOneH()
+function increment(type, value)
 {
-    score_home += 1;
-    homeScore.textContent = score_home;
-}
-
-function incrementTwoH()
-{
-    score_home += 2;
-    homeScore.textContent = score_home;
-}
-
-function incrementThreeH()
-{
-    score_home += 3;
-    homeScore.textContent = score_home;
-}
-
-function incrementOneG()
-{
-    score_guest += 1;
-    guestScore.textContent = score_guest;
-}
-
-function incrementTwoG()
-{
-    score_guest += 2;
-    guestScore.textContent = score_guest;
-}
-
-function incrementThreeG()
-{
-    score_guest += 3;
-    guestScore.textContent = score_guest;
+    if (type == 'home') {
+        score_home += value;
+        homeScore.textContent = score_home;
+    } else {
+        score_guest += value;
+        guestScore.textContent = score_guest;
+    }
 }
