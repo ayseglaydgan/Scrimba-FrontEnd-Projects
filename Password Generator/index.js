6c73ed0a13a910548db926d861482a09b1a52096
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-10-26 22:31:24 +0300

    width value added to non-alpha button

--- Password Generator/index.js
+++ Password Generator/index.js
@@ -1,6 +1,5 @@
-
 const character_1 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]; //91 characters
+"/"]; 
 
 const character_2 = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -56,7 +55,7 @@ async function copyToClipboard() {
     flag = !flag;
     
     nonAlpha.textContent = 
-            !flag ? "DONT INCLUDE SYMBOLS AND NUMBERS" 
+            !flag ? "DON'T INCLUDE SYMBOLS AND NUMBERS" 
                 : "INCLUDE SYMBOLS AND NUMBERS";
 }
 
