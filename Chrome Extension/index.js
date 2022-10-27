3d3d01a0b77655b916cdc6c07d6fc93a956b54b2
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-10-28 00:30:22 +0300

    innerHTML function tried

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,5 +1,14 @@
-let inputBtn = document.getElementById('input-btn');
+let myLeads = ["www.google.com", "www.facebook.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById("ul-el"); 
 
 inputBtn.addEventListener('click', function() {
-    console.log('inputBtn clicked');
-}); 
+    myLeads.push(inputEl.value);
+    console.log(myLeads);
+    
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
