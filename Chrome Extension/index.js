077f5ebddf8812564b849f6800cfbf8fecc51252
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-10-31 15:46:04 +0300

    template string added

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,25 +1,31 @@
-let myLeads = ["www.google.com", "www.facebook.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById("ul-el"); 
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value);
-    console.log(myLeads);
+    renderLeads();
+    inputEl.value = "";
     
 })
 
 function renderLeads(){
-let listItems = "";
+    let listItems = "";
 
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
 
-    listItems += "<li>" + myLeads[i] + "</li>";
-
-   /*const li = document.createElement("li");
-    li.textContent = myLeads[i];
-    ulEl.append(li);*/
-}
-ulEl.innerHTML = listItems;
+    /*const li = document.createElement("li");
+        li.textContent = myLeads[i];
+        ulEl.append(li);*/
+    }
+    ulEl.innerHTML = listItems;
+    
 }
