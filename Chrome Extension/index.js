cde5e9d457548d289804e40f6a382f9e53006853
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-01 19:49:12 +0300

    addHttpsIfMissing function added

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -3,17 +3,28 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById("ul-el"); 
 
+const addHttpsIfMissing = (url) => {
+    if (url.startsWith('http://') || url.startsWith('https://')) {
+        return url;
+    } else {
+        return 'https://' + url;
+    }
+}
+
 inputBtn.addEventListener('click', function() {
-    myLeads.push(inputEl.value);
+    myLeads.push(addHttpsIfMissing(inputEl.value));
     renderLeads();
     inputEl.value = "";
     
 })
 
+
+
 function renderLeads(){
     let listItems = "";
-
+    console.log(myLeads);
     for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i]);
         listItems += `
         <li>
             <a target='_blank' href='${myLeads[i]}'>
