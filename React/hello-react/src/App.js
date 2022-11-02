d16733d901f8a6293e08c628d5d5b1af69f64c86
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-02 15:32:52 +0300

    render variables with components

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 import Header from './components/Header';
 
+const name="John";
+const age=30;
+
+
 function App() {
   return (
     <div className="App">
+      {name} is {age} years old.
       <div>
         <h1>merhaba</h1>
         <Header />
