d7c64608520e0584a1c093c4881474d9ea0c3fbd
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-02 15:30:17 +0300

    JSX examples tried

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -5,9 +5,14 @@ import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>merhaba</h1>
-      <Header />
+    <div className="App">
+      <div>
+        <h1>merhaba</h1>
+        <Header />
+      </div>
+      <div>
+        <p>second div</p>
+      </div>
     </div>
   );
 }
