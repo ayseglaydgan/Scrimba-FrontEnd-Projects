27c36cd1af9985cfa470138761060bb4de957787
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-02 15:18:29 +0300

    first react project initialized

---
+++ React/hello-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <h1>merhaba</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
