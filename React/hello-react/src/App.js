ce11fada9d4004044c78b7ed7e5be78ee031bccb
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-08 16:09:52 +0300

    New "Login" component added.

--- React/hello-react/src/App.js
+++ React/hello-react/src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
-
+import React from 'react';
 import Header from './components/Header';
+import Login from './components/Login';
 
 const name="John";
 const age=30;
+const isLoggedin=false;
 
 
 function App() {
-  return (
-    <div className="App">
-      {name} is {age} years old.
-      <div>
-        <h1>merhaba</h1>
-        <Header />
-      </div>
-      <div>
-        <p>second div</p>
-      </div>
+  return <div className="App">
+    {/* { isLoggedin ? <h1>Hello {name}</h1> : <h1>Hello Guest</h1> } */
+    isLoggedin && <h1>Hello {name}</h1>
+    }
+    {
+      !isLoggedin && <div> <h1>Hello Guest</h1> <Login /> </div>
+    }
     </div>
-  );
+  
 }
 
 export default App;
