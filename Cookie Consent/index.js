d7c086ef7104c6b189c9545de99e66b803e28578
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-27 19:01:54 +0300

    FormData() used

--- Cookie Consent/index.js
+++ Cookie Consent/index.js
@@ -3,9 +3,10 @@ const modalEl = document.getElementById('modal');
 const closeEl = document.getElementById('modal-close-btn');
 const formEl = document.getElementById('consent-form');
 const modalTextEl = document.getElementById('modal-text');
-const acceptBtnEl = document.getElementById('accept-btn');
 
-console.log("acceptBtnEl", acceptBtnEl.innerHTML);
+setTimeout(function(){
+    modalEl.style.display = 'inline';
+}, 3000)
 
 
 closeEl.addEventListener('click', function(){
@@ -14,14 +15,17 @@ closeEl.addEventListener('click', function(){
 
 formEl.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log('form submitted');
-});
 
-setTimeout(function(){
-    modalEl.style.display = 'inline';
-}, 3000)
+    const formData = new FormData(formEl);
+    console.log(formData);
+    const name = formData.get('name');
+    console.log(name);
+    const email = formData.get('email');
+    console.log(email);
+    
 
-acceptBtnEl.addEventListener('click', function(){
+
+
     let newModalText = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -30,6 +34,7 @@ acceptBtnEl.addEventListener('click', function(){
         </p>
     </div>`
     modalTextEl.innerHTML = newModalText;
+
     setTimeout(function(){
         document.getElementById('upload-text').textContent = `Making the sale...`
     }, 1500);
@@ -42,8 +47,9 @@ acceptBtnEl.addEventListener('click', function(){
         </div>
         `
     },3000);
-
     
-
 });
+
+
+
 
