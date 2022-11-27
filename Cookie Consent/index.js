1621d7bfef9085f8c0e1616924dca6f611854953
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-11-27 15:49:13 +0300

    decline button fixed

--- Cookie Consent/index.js
+++ Cookie Consent/index.js
@@ -1,10 +1,16 @@
 
 const modalEl = document.getElementById('modal');
 const closeEl = document.getElementById('modal-close-btn');
+const formEl = document.getElementById('consent-form');
 
 
 closeEl.addEventListener('click', function(){
   modalEl.style.display = 'none';
+});
+
+formEl.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('form submitted');
 });
 
 setTimeout(function(){
