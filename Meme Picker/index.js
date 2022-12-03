8debe88623ece7e91abd348d8801a0c231be934a
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-12-03 22:11:13 +0300

    event.target.id taken

--- Meme Picker/index.js
+++ Meme Picker/index.js
@@ -1,13 +1,19 @@
-import {catsData} from '/data.js'
+import { catsData } from './data.js';
 
 const emotionRadiosEl = document.getElementById('emotion-radios')
+
+emotionRadiosEl.addEventListener('change', function(e){
+    console.log(e.target.id);
+})
 
 
 function getEmotionsArray(cats){
     let emotionsArray = [];
     for(let cat of catsData){
         for(let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if(!emotionsArray.includes(emotion)){
+                emotionsArray.push(emotion);
+            }
         }
     }
     return emotionsArray;
@@ -17,7 +23,16 @@ function renderEmotionsRadios(cats){
     let emotionRadiosString = "";
     const emotions = getEmotionsArray(cats);
     for(let emotion of emotions){
-        emotionRadiosString += `<p>${emotion}</p>`
+        emotionRadiosString += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+                <input 
+                type='radio' 
+                id="${emotion}"
+                value='${emotion}'
+                name="emotions">
+        </div>
+        `
     }
     emotionRadiosEl.innerHTML = emotionRadiosString;
     console.log(emotionRadiosString)
