cac9c8a4c93e5a5ae9299e45c6287ac020ae9213
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-12-05 01:51:49 +0300

    like, retweet and replies buttons added

--- Twitter Clone/index.js
+++ Twitter Clone/index.js
@@ -2,7 +2,6 @@ import { tweetsData } from './data.js';
 
 const tweetInput = document.getElementById('tweet-input');
 const tweetButton = document.getElementById('tweet-btn');
-const feed = document.getElementById('feed');
 
 tweetButton.addEventListener('click', function(){
     console.log(tweetInput.value);
@@ -10,7 +9,7 @@ tweetButton.addEventListener('click', function(){
 
 function getFeedHtml(tweets){
     let tweetHtml = '';
-    for(let tweet of tweets){
+    tweets.forEach(function (tweet) {
         tweetHtml += `
         <div class="tweet">
             <div class="tweet-inner">
@@ -20,20 +19,27 @@ function getFeedHtml(tweets){
                     <p class="tweet-text">${tweet.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-comment-dots fa-regular"></i>
                             ${tweet.replies.length}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-heart fa-solid"></i>
                             ${tweet.likes}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-retweet fa-solid"></i>
                             ${tweet.retweets}
                         </span>
                     </div>   
                 </div>            
             </div>
     </div>`
-    }
-    feed.innerHTML = tweetHtml;
+    });
+    return tweetHtml;
 }
 
-getFeedHtml(tweetsData);
+function renderFeed(){
+    document.getElementById('feed').innerHTML = getFeedHtml(tweetsData);
+}
+
+renderFeed();
