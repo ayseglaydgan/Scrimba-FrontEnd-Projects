2da7a3005a2506879c964f0e5d699380a1eba6e4
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2022-12-04 18:04:45 +0300

    feed implemented

--- Twitter Clone/index.js
+++ Twitter Clone/index.js
@@ -1,1 +1,39 @@
-import { tweetsData } from "./data";
+import { tweetsData } from './data.js';
+
+const tweetInput = document.getElementById('tweet-input');
+const tweetButton = document.getElementById('tweet-btn');
+const feed = document.getElementById('feed');
+
+tweetButton.addEventListener('click', function(){
+    console.log(tweetInput.value);
+});
+
+function getFeedHtml(tweets){
+    let tweetHtml = '';
+    for(let tweet of tweets){
+        tweetHtml += `
+        <div class="tweet">
+            <div class="tweet-inner">
+                <img src="${tweet.profilePic}" class="profile-pic">
+                <div>
+                    <p class="handle">${tweet.handle}</p>
+                    <p class="tweet-text">${tweet.tweetText}</p>
+                    <div class="tweet-details">
+                        <span class="tweet-detail">
+                            ${tweet.replies.length}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.likes}
+                        </span>
+                        <span class="tweet-detail">
+                            ${tweet.retweets}
+                        </span>
+                    </div>   
+                </div>            
+            </div>
+    </div>`
+    }
+    feed.innerHTML = tweetHtml;
+}
+
+getFeedHtml(tweetsData);
