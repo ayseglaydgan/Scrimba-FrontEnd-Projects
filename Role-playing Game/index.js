865fda1ba395d9429c708be785b9929a6aee7f76
Ayşegül Aydoğan <aysegul.99@windowslive.com>
2023-01-04 14:14:13 +0300

    character file added

--- Role-playing Game/index.js
+++ Role-playing Game/index.js
@@ -1,39 +1,19 @@
 import characterData from "./data.js";
-
-import { getDiceRollArray } from "./utils.js";
-
-function Character(data) {
-   Object.assign(this, data)
-
-   this.getDiceHtml = function (diceCount) {
-      return getDiceRollArray(diceCount).map(function (num) {
-         return `<div class="dice">${num}</div>`
-      }).join('')
-   }
-
-   this.getCharacterHtml = function () {
-      const { elementId, name, avatar, health, diceCount } = this;
-      let diceHtml = this.getDiceHtml(diceCount);
-
-      return `
-           <div class="character-card">
-               <h4 class="name"> ${name} </h4>
-               <img class="avatar" src="${avatar}" />
-               <div class="health">health: <b> ${health} </b></div>
-               <div class="dice-container">
-                   ${diceHtml}
-               </div>
-           </div>`;
-
-   }
-}
+import Character from "./character.js";
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
 
+function attack() {
+   console.log("button clicked")
+   render()
+}
+
+document.getElementById('attack-button').addEventListener('click', attack)
+
 function render() {
-   document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
-   document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
+   document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+   document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 }
 render()
 
